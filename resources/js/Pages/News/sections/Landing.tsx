f994923184fa4e387bad0e461324ca956df60c8c
mamelilea <achmalpradiptaaditama@gmail.com>
2024-12-31 15:56:21 +0700

    style: adjust text positioning on landing

--- resources/js/Pages/News/sections/Landing.tsx
+++ resources/js/Pages/News/sections/Landing.tsx
@@ -1,7 +1,7 @@
 const Landing = () => {
     return (
         <div className="text-deep-blue flex min-h-[115vh] w-full items-center bg-[url(/images/bg-LandingNews.png)] bg-cover bg-top bg-no-repeat font-poppins justify-center">
-            <div className="w-[80%] flex flex-col gap-5">
+            <div className="w-[80%] flex flex-col gap-5 mb-20">
                 <h1 className="text-5xl font-extrabold">BERITA</h1>
                 <h2 className="text-4xl font-semibold">
                     YAYASAN LEMBAGA MIRYAM

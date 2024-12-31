14af49e4f51467ccd93ec441812bdaabb3cd7466
nathakusuma <nathakusuma07@gmail.com>
2025-01-01 01:00:55 +0700

    feat(berita): integrate sorotan and teratas

--- resources/js/models/newsinterfaces.ts
+++ resources/js/models/newsinterfaces.ts
@@ -1,3 +1,13 @@
+export interface Berita {
+    id: number;
+    slug: string;
+    judul: string;
+    isi: string;
+    gambar_url: string;
+    created_at: string;
+    updated_at: string;
+}
+
 export interface NewsItem {
     image: string;
     title: string;

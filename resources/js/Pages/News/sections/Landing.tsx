2ff4286cf1837f0cd4724fcd21c37e8521326beb
mamelilea <achmalpradiptaaditama@gmail.com>
2024-12-31 15:18:59 +0700

    `refactor:` revise news and detail page

--- resources/js/Pages/News/sections/Landing.tsx
+++ resources/js/Pages/News/sections/Landing.tsx
@@ -1,9 +1,9 @@
 const Landing = () => {
     return (
-        <div className="text-deep-blue flex min-h-[110vh] w-full items-center bg-[url(/images/bg-LandingNews.webp)] bg-cover bg-top bg-no-repeat font-poppins justify-center">
+        <div className="text-deep-blue flex min-h-[115vh] w-full items-center bg-[url(/images/bg-LandingNews.png)] bg-cover bg-top bg-no-repeat font-poppins justify-center">
             <div className="w-[80%] flex flex-col gap-5">
-                <h1 className="text-7xl font-extrabold">BERITA</h1>
-                <h2 className="text-6xl font-semibold">
+                <h1 className="text-5xl font-extrabold">BERITA</h1>
+                <h2 className="text-4xl font-semibold">
                     YAYASAN LEMBAGA MIRYAM
                 </h2>
             </div>

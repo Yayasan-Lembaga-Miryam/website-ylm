5c77018ba8144174d6c09227af7b1c316419e600
nathakusuma <nathakusuma07@gmail.com>
2025-01-01 01:00:55 +0700

    feat(berita): integrate sorotan and teratas

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -5,6 +5,21 @@ export interface User {
     email_verified_at?: string;
 }
 
+export type PaginatedData<T = never> = {
+    total: number;
+    per_page: number;
+    current_page: number;
+    last_page: number;
+    first_page_url: string | null;
+    last_page_url: string | null;
+    next_page_url: string | null;
+    prev_page_url: string | null;
+    path: string;
+    from: number;
+    to: number;
+    data: T[];
+};
+
 export type PageProps<
     T extends Record<string, unknown> = Record<string, unknown>,
 > = T & {

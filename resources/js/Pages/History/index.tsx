568645a683cef2bdad068723dfdc797804b07a3f
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-01 17:27:44 +0700

    `feat(sejarah):` finish slicing history page

---
+++ resources/js/Pages/History/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/Layout';
+import Landing from './sections/Landing';
+import Desc from './sections/Desc';
+
+const History = () => {
+    return (
+        <Layout>
+            <Landing />
+            <Desc/>
+        </Layout>
+    );
+};
+
+export default History;

e90d49af1c248be708885c9f8bac7708d81bf8dc
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-01 23:19:49 +0700

    `feat(kurikulum):` complete slicing of curriculum page

---
+++ resources/js/Pages/Curriculum/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/Layout';
+import Landing from './sections/Landing';
+import List from './sections/List';
+
+const Curriculum = () => {
+    return (
+        <Layout>
+            <Landing />
+            <List />
+        </Layout>
+    );
+};
+
+export default Curriculum;
---
+++ resources/js/Pages/Curriculum/sections/Landing.tsx
@@ -0,0 +1,14 @@
+const Landing = () => {
+    return (
+        <div className="flex min-h-[115vh] w-full items-center justify-center bg-[url(/images/bg-LandingCurriculum.png)] bg-cover bg-center bg-no-repeat font-poppins text-white">
+            <div className="mb-20 flex w-[80%] flex-col gap-5">
+                <h1 className="text-5xl font-extrabold">KURIKULUM</h1>
+                <h2 className="text-4xl font-semibold">
+                    YAYASAN LEMBAGA MIRYAM
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Landing;

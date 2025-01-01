11eee96d2a4a3f08b3aae3bcd0f098f1276d664d
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-01 17:27:44 +0700

    `feat(sejarah):` finish slicing history page

---
+++ resources/js/Pages/History/sections/Landing.tsx
@@ -0,0 +1,14 @@
+const Landing = () => {
+    return (
+        <div className="text-white flex min-h-screen w-full items-center justify-center bg-[url(/images/bg-LandingHistory.png)] bg-cover bg-top bg-no-repeat font-poppins">
+            <div className="mb-20 flex w-[80%] flex-col gap-5 items-center">
+                <h1 className="text-5xl font-extrabold">SEJARAH</h1>
+                <h2 className="text-4xl font-semibold">
+                    YAYASAN LEMBAGA MIRYAM
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Landing;

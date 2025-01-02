1477b18fbda01f421e4081cd23a6113ea1e71874
nathakusuma <nathakusuma07@gmail.com>
2025-01-03 00:08:58 +0700

    fix(berita): fix summary cuts on newline

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Berita;
+use Illuminate\Support\Str;
 use Inertia\Response;
 
 class HomePageController extends Controller
 {
     public function index(): Response
     {
-        $berita = Berita::query()->orderBy('id', 'desc')->limit(12)->get();
+        $berita = Berita::query()->orderBy('id', 'desc')->limit(12)->get()
+            ->transform(function ($berita) {
+                $berita->isi = str_replace("\\n", " ", Str::limit($berita->isi, 110));
+                return $berita;
+            });
 
         $props = [
             'berita' => $berita,

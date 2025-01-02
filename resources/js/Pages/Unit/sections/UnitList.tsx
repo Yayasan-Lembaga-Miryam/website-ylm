f288c2cf82c8cc3673c1a1a2cb7ce1c6cc4a85e9
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-02 18:34:55 +0700

    `feat(unit-belajar):` complete slicing of unit belajar page

---
+++ resources/js/Pages/Unit/sections/UnitList.tsx
@@ -0,0 +1,57 @@
+import UnitCard from '@/Components/Unit/UnitCard';
+
+interface Props {
+    units: {
+        nama: string;
+        image: string;
+        lokasi: string;
+        slug: string;
+        type:
+            | 'taman kanak kanak'
+            | 'sekolah dasar'
+            | 'sekolah menengah pertama';
+    }[];
+}
+
+const UnitList = ({ units }: Props) => {
+    const filterUnitsByType = (type: Props['units'][0]['type']) => {
+        return units.filter((unit) => unit.type === type);
+    };
+
+    return (
+        <div className="-mt-[90px] flex min-h-screen w-full justify-center bg-[url(/images/bg-UnitListUnit.webp)] bg-cover bg-top bg-no-repeat py-20 font-poppins text-deep-blue">
+            <div className="mt-64 w-[80%] flex flex-col gap-12 items-center justify-center">
+                <div className="space-y-5">
+                    <h1 className="text-3xl font-extrabold">
+                        Taman Kanak-Kanak
+                    </h1>
+                    <div className="flex flex-wrap gap-12">
+                        {filterUnitsByType('taman kanak kanak').map((unit) => (
+                            <UnitCard key={unit.slug} unit={unit} />
+                        ))}
+                    </div>
+                </div>
+                <div className="space-y-5">
+                    <h1 className="text-3xl font-extrabold">Sekolah Dasar</h1>
+                    <div className="flex flex-wrap gap-12">
+                        {filterUnitsByType('sekolah dasar').map((unit) => (
+                            <UnitCard key={unit.slug} unit={unit} />
+                        ))}
+                    </div>
+                </div>
+                <div className="space-y-5">
+                    <h1 className="text-3xl font-extrabold">
+                        Sekolah Menengah Pertama
+                    </h1>
+                    <div className="flex flex-wrap gap-12">
+                        {filterUnitsByType('sekolah menengah pertama').map((unit) => (
+                            <UnitCard key={unit.slug} unit={unit} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UnitList;

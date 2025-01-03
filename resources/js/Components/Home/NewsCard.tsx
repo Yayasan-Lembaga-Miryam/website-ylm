a4362c15915b220e894f62fcf38c7e8585c6d271
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-04 00:38:26 +0700

    `feat(berita):` Enhance NewsCard on HomePage to redirect users to the NewsDetail page

--- resources/js/Components/Home/NewsCard.tsx
+++ resources/js/Components/Home/NewsCard.tsx
@@ -1,12 +1,26 @@
+import { router } from '@inertiajs/react';
+
 interface NewsCardProps {
     image: string;
     title: string;
     description: string;
+    slug: string;
 }
 
-const NewsCard: React.FC<NewsCardProps> = ({ image, title, description }) => {
+const NewsCard: React.FC<NewsCardProps> = ({
+    image,
+    title,
+    description,
+    slug,
+}) => {
+    const handleNewsClick = (slug: string) => {
+        router.visit(`/berita/${slug}`);
+    };
     return (
-        <div className="mx-2 flex flex-col overflow-hidden rounded-lg bg-white shadow-lg font-poppins">
+        <div
+            className="mx-2 flex cursor-pointer flex-col overflow-hidden rounded-lg bg-white font-poppins shadow-lg active:scale-95 active:border active:border-black"
+            onClick={() => handleNewsClick(slug)}
+        >
             <div className="h-44 overflow-hidden">
                 <img
                     src={image}
@@ -15,11 +29,13 @@ const NewsCard: React.FC<NewsCardProps> = ({ image, title, description }) => {
                 />
             </div>
             <div className="px-6 py-3">
-                <h3 className="mb-2 line-clamp-2 font-semibold">{title}</h3>
-                <p className="line-clamp-2 text-gray-600 text-[10px]">{description}</p>
+                <h3 className="mb-2 line-clamp-1 font-semibold">{title}</h3>
+                <p className="line-clamp-3 text-[10px] text-gray-600">
+                    {description}
+                </p>
             </div>
         </div>
     );
 };
 
-export default NewsCard
+export default NewsCard;

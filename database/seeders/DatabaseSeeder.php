ea7881a2c6dbc0c6848abf2cc3fd19776fb43b3e
nathakusuma <nathakusuma07@gmail.com>
2025-01-03 22:41:22 +0700

    chore(admin): add admin super & admin unit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         Berita::factory(100)->create();
         Berita::factory(5)->recent()->create();
         Berita::factory(3)->withoutImage()->create();

d5c4c5027e67c34854b88bb8982e8f921620f8a8
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-04 18:41:21 +0700

    `refactor:` adjust Navbar and Layout to conditionally render based on admin and login status

--- resources/js/Layout/index.tsx
+++ resources/js/Layout/index.tsx
@@ -3,12 +3,14 @@ import Navbar from '@/Components/Shared/Navbar';
 
 type LayoutProps = {
     children: React.ReactNode;
+    isAdmin?: boolean;
+    isLogin?: boolean;
 };
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, isAdmin, isLogin }: LayoutProps) => {
     return (
         <div className='overflow-x-hidden font-poppins'>
-            <Navbar />
+            <Navbar isAdmin={isAdmin} isLogin={isLogin} />
             {children}
             <Footer />
         </div>

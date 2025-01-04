023823d5d570f3ab1086c6c1d24c5132130646a8
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-04 08:33:07 +0700

    `feat(navbar):` Adjust Navbar to handle user and admin login states

--- resources/js/Components/Shared/Navbar.tsx
+++ resources/js/Components/Shared/Navbar.tsx
@@ -1,11 +1,16 @@
-import { Link } from '@inertiajs/react';
+import { Link, router } from '@inertiajs/react';
 
 interface NavItem {
     name: string;
     route: string;
 }
 
-const Navbar = () => {
+type NavbarProps = {
+    isAdmin?: boolean;
+    isLogin?: boolean;
+};
+
+const Navbar = ({ isAdmin = false, isLogin = false }: NavbarProps) => {
     const navItems: NavItem[] = [
         { name: 'Beranda', route: '/' },
         { name: 'Tentang', route: '/tentang' },
@@ -14,28 +19,50 @@ const Navbar = () => {
         { name: 'Unit Belajar', route: '/unit-belajar' },
         { name: 'Kurikulum', route: '/kurikulum' },
     ];
+    const handleLogout = () => {
+        router.post(route('logout'));
+    };
     return (
         <nav className="fixed top-0 z-50 flex h-28 w-full items-center justify-center bg-transparent">
-            <div className="h-[70%] w-[90%] flex justify-center rounded-2xl bg-dark-blue px-4">
-                <div className='flex h-full justify-between w-[90%]'>
+            <div className="flex h-[70%] w-[90%] justify-center rounded-2xl bg-dark-blue px-4">
+                <div className="flex h-full w-[90%] justify-between">
                     <Link href="/" className="flex h-full items-center gap-3">
                         <img
-                            src="/images/img-Logo.png"
+                            src="/images/img-LogoText.png"
                             alt="Yayasan Lembaga Miryam"
                             className="h-[80%]"
                         />
                     </Link>
-                    <div className="flex items-center gap-8">
-                        {navItems.map((item) => (
+                    {!isAdmin && isLogin && (
+                        <div className="flex items-center gap-8">
+                            {navItems.map((item) => (
+                                <Link
+                                    key={item.name}
+                                    href={item.route}
+                                    className="font-semibold text-white transition-colors duration-200 hover:text-gray-200"
+                                >
+                                    {item.name}
+                                </Link>
+                            ))}
+                        </div>
+                    )}
+                    {isAdmin && (
+                        <div className="flex items-center gap-8">
                             <Link
-                                key={item.name}
-                                href={item.route}
-                                className="text-white transition-colors duration-200 hover:text-gray-200 font-semibold"
+                                href="/dashboard"
+                                className="font-semibold text-white transition-colors duration-200 hover:text-gray-200"
                             >
-                                {item.name}
+                                Beranda
                             </Link>
-                        ))}
-                    </div>
+                            <button
+                                onClick={handleLogout}
+                                type="button"
+                                className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
         </nav>

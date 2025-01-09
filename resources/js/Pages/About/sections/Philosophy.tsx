b75ac9123c7f508d5811bc764bd61f197f8597ea
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-09 22:52:22 +0700

    fix: reduce bottom padding for Filosofi section

--- resources/js/Pages/About/sections/Philosophy.tsx
+++ resources/js/Pages/About/sections/Philosophy.tsx
@@ -2,7 +2,7 @@ import Carousel from '@/Components/About/PhilosophyCarousel';
 
 const Philosophy = () => {
     return (
-        <div className="my-20 flex h-full min-h-screen w-[80%] flex-col items-center gap-12 rounded-[20px] text-dark-blue">
+        <div className="mt-20 flex h-full min-h-screen w-[80%] flex-col items-center gap-12 rounded-[20px] text-dark-blue">
             <h1 className="text-3xl font-extrabold">Filosofi Logo</h1>
             <div className='flex justify-center'>
                 <Carousel />

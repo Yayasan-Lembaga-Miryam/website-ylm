03ace5640954e78f0037b50f5b1378ddc51d5b69
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-11 13:58:34 +0700

    refactor(about): update history section to use Inertia Link for navigation

--- resources/js/Pages/About/sections/History.tsx
+++ resources/js/Pages/About/sections/History.tsx
@@ -1,11 +1,12 @@
 import Button from '@/Components/Shared/Button';
+import { Link } from '@inertiajs/react';
 import { FaArrowRight } from 'react-icons/fa6';
 import Founder from './Founder';
 import Philosophy from './Philosophy';
 
 const History = () => {
     return (
-        <div className="-mt-[75px] flex min-h-screen w-full flex-col items-center justify-center bg-[url(/images/bg-HistoryAbout.webp)] bg-cover bg-top bg-no-repeat font-poppins relative">
+        <div className="relative -mt-[75px] flex min-h-screen w-full flex-col items-center justify-center bg-[url(/images/bg-HistoryAbout.webp)] bg-cover bg-top bg-no-repeat font-poppins">
             <div className="z-10 mt-64 flex w-[80%] flex-col items-center justify-center gap-16 py-20 text-deep-blue">
                 <h1 className="text-3xl font-extrabold">
                     Sejarah Yayasan Lembaga Miryam
@@ -23,20 +24,19 @@ const History = () => {
                             Kongregasi memulai karya misi di Sumatera bagian
                             Selatan. Setelah diperoleh jawaban....
                         </p>
-                        <Button
-                            appearance="outline"
-                            display="text-icon"
-                            icon={<FaArrowRight />}
-                            iconPosition="right"
-                            type="button"
-                            variant="primary"
-                            onClick={() => {
-                                window.location.href = '/sejarah';
-                            }}
-                            className="w-max rounded-xl border-dark-blue font-poppins text-dark-blue hover:border-dark-blue hover:bg-dark-blue hover:text-white"
-                        >
-                            Lihat Selengkapnya
-                        </Button>
+                        <Link href="/sejarah">
+                            <Button
+                                appearance="outline"
+                                display="text-icon"
+                                icon={<FaArrowRight />}
+                                iconPosition="right"
+                                type="button"
+                                variant="primary"
+                                className="w-max rounded-xl border-dark-blue font-poppins text-dark-blue hover:border-dark-blue hover:bg-dark-blue hover:text-white"
+                            >
+                                Lihat Selengkapnya
+                            </Button>
+                        </Link>
                     </div>
                     <div className="flex w-1/2 flex-col items-end">
                         <img

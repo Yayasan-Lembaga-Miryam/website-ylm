0aeb3778fc43db52c884b13aa07c8d67e043e548
nathakusuma <nathakusuma07@gmail.com>
2025-01-12 04:34:18 +0700

    feat(pengurus): add update pengurus (admin)

--- app/Http/Controllers/PengurusController.php
+++ app/Http/Controllers/PengurusController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PengurusUpdateRequest;
 use App\Models\Pengurus;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Response;
 
 class PengurusController extends Controller
@@ -15,5 +18,26 @@ class PengurusController extends Controller
 
         //TODO: return inertia
         dd($props);
+    }
+
+    public function update(PengurusUpdateRequest $request, Pengurus $pengurus): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        $pengurus->update($validated);
+
+        if ($request->hasFile('foto')) {
+            // save file, replace the foto_path with the new path, and delete the old file
+            $newFotoPath = $request->file('foto')->store('pengurus/images', 'public');
+
+            if ($pengurus->foto_path) {
+                Storage::disk('public')->delete($pengurus->foto_path);
+            }
+
+            $pengurus->foto_path = $newFotoPath;
+            $pengurus->save();
+        }
+
+        return redirect()->back()->with('message', 'Pengurus berhasil diubah');
     }
 }

ac831229ca5c07cd9bf7f94e9510c9848ea3b433
nathakusuma <nathakusuma07@gmail.com>
2025-01-12 04:34:18 +0700

    feat(pengurus): add update pengurus (admin)

---
+++ app/Http/Requests/PengurusUpdateRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PengurusUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->user()?->isAdminSuper() ?? false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nama' => ['required', 'string', 'max:50'],
+            'jabatan' => ['required', 'string', 'max:50'],
+            'keterangan_jabatan' => ['required', 'string', 'max:50'],
+            'foto' => ['nullable', 'image', 'max:2048'],
+        ];
+    }
+}

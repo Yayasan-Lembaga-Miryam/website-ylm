19d891e721256e62b65c496aaba5d2b9a438e661
nathakusuma <nathakusuma07@gmail.com>
2025-01-12 04:32:04 +0700

    feat(pengurus): add migration and model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
             AdminUserSeeder::class,
             BeritaSeeder::class,
             GaleriSeeder::class,
+            PengurusSeeder::class,
         ]);
     }
 }

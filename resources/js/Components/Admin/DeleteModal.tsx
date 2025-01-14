87627ed47c837d7550e71c8ffa6a76a8c351c975
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-15 02:25:54 +0700

    refactor: make delete modal reusable

--- resources/js/Components/Admin/DeleteModal.tsx
+++ resources/js/Components/Admin/DeleteModal.tsx
@@ -6,15 +6,17 @@ interface DeleteModalProps {
     show: boolean;
     onClose: () => void;
     onDeleteConfirm: () => void;
+    type : "news" | "album" | "foto";
+    isLoading?: boolean;
 }
 
-const DeleteModal = ({ show, onClose, onDeleteConfirm }: DeleteModalProps) => {
+const DeleteModal = ({ show, onClose, onDeleteConfirm, type }: DeleteModalProps) => {
     return (
         <Modal maxWidth="sm" show={show} onClose={onClose}>
             <div className="flex flex-col items-center justify-center gap-4 p-6">
                 <FaTrash className="size-10 text-dark-blue" />
                 <h2 className="text-lg font-semibold">
-                    Yakin mau menghapus berita ini?
+                Yakin mau menghapus {type === "news" ? "berita" : type === "album" ? "album" : type === "foto" ? "foto" : "item"} ini?
                 </h2>
                 <div className="flex justify-end space-x-3">
                     <Button

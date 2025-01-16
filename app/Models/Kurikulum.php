1d45fcd7c039732dbe305846f98aa444b33cc331
nathakusuma <nathakusuma07@gmail.com>
2025-01-16 22:59:26 +0700

    feat(kurikulum): add migration and model

---
+++ app/Models/Kurikulum.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kurikulum extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kurikulum';
+
+    protected $fillable = [
+        'judul',
+        'url',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+}

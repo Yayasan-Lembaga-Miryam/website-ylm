156776a7abdcb0ccbf42ec5dcb09e94005679d86
nathakusuma <nathakusuma07@gmail.com>
2025-01-16 23:51:49 +0700

    feat(kurikulum): integrate kurikulum index with backend

--- resources/js/Pages/Curriculum/sections/List.tsx
+++ resources/js/Pages/Curriculum/sections/List.tsx
@@ -1,3 +1,5 @@
+import { KurikulumItem } from "@/models/kurikulum";
+
 const data = [
     {
         id: 1,
@@ -56,18 +58,19 @@ const data = [
     },
 ];
 
-const List = () => {
+const List = ({ kurikulum }: { kurikulum: KurikulumItem[] }) => {
     return (
         <div className="-mt-[80px] flex min-h-screen w-full justify-center bg-[url(/images/bg-ListCurriculum.webp)] bg-cover bg-top bg-no-repeat font-poppins">
             <div className="mt-64 flex w-[80%] flex-col items-center justify-center gap-12 pb-20">
-                {data.map((item) => (
+                {kurikulum.map((item: KurikulumItem) => (
                     <a
                         key={item.id}
-                        href={item.link}
+                        href={item.url}
                         target="_blank"
                         className="w-[60%] rounded-[20px] bg-dark-blue py-4 text-center text-lg font-semibold text-white shadow-md hover:bg-white hover:text-black"
+                        rel="noreferrer"
                     >
-                        {item.title}
+                        {item.judul}
                     </a>
                 ))}
             </div>

a7807436cc25ed28984040a6c7c8198bf06442ff
nathakusuma <nathakusuma07@gmail.com>
2025-01-16 17:51:50 +0700

    feat(unit): add update pengurus unit

---
+++ app/Http/Requests/PengurusUnitUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PengurusUnitUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->user()->isAdminSuper() || auth()->user()->getAdminUnit() === $this->unit->slug;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category' => 'required|string|in:kepala,guru,tenaga-kependidikan',
+            'nama' => 'required|string|max:50',
+            'jabatan' => 'required|string|max:50',
+            'foto' => 'nullable|image|max:2048',
+            'prioritas' => 'required|integer',
+        ];
+    }
+}

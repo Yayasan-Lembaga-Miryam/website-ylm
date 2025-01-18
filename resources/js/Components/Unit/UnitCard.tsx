191ede6f1efe350f675ae93ae04fa44a09483484
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-18 08:33:55 +0700

    feat(unit): categorize units based on slug for user display

--- resources/js/Components/Unit/UnitCard.tsx
+++ resources/js/Components/Unit/UnitCard.tsx
@@ -3,13 +3,9 @@ import { Link } from '@inertiajs/react';
 interface UnitCardProps {
     unit: {
         nama: string;
-        image: string;
-        lokasi: string;
+        thumbnail_url: string;
+        alamat_singkat: string;
         slug: string;
-        type:
-            | 'taman kanak kanak'
-            | 'sekolah dasar'
-            | 'sekolah menengah pertama';
     };
 }
 
@@ -18,13 +14,13 @@ const UnitCard = ({ unit }: UnitCardProps) => {
         <Link href={`/unit/${unit.slug}`}>
             <div className="relative h-[50vh] max-h-[300px] w-[40vw] max-w-[500px] rounded-xl">
                 <img
-                    src={unit.image}
+                    src={unit.thumbnail_url}
                     alt={unit.nama}
                     className="h-full w-full rounded-xl"
                 />
                 <div className="absolute bottom-0 right-0 top-0 flex w-[55%] flex-col items-end justify-center gap-2 rounded-xl bg-[url(/images/bg-CardUnit.png)] bg-cover px-3 font-bold text-white">
                     <h1 className="w-[80%] text-xl">{unit.nama}</h1>
-                    <h2 className="w-[80%] text-[10px]">{unit.lokasi}</h2>
+                    <h2 className="w-[80%] text-[10px]">{unit.alamat_singkat}</h2>
                 </div>
             </div>
         </Link>

94096004c2d7ed415d1850218f653f6909ceff86
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-19 18:05:56 +0700

    feat(admin-unit): initial setup for admin unit page

---
+++ resources/js/Components/Admin/Unit/GuruSection.tsx
@@ -0,0 +1,13 @@
+export const GuruSection = () => {
+    return (
+        <div className="w-full">
+            <div className="w-full space-y-5 text-dark-blue">
+                <h1 className="text-3xl font-bold">Update Jabatan Guru</h1>
+                <p>
+                    Untuk melakukan perbaruan pada pemangku jabatan pada Guru,
+                    dengan detail mencantumkan nama dan foto pemangku jabatan.
+                </p>
+            </div>
+        </div>
+    );
+};

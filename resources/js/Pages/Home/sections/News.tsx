83a4baf46e56a91bf53fc35d049a2c83032d9897
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-19 17:43:20 +0700

    feat(home): remove 'newpost' section from home page

--- resources/js/Pages/Home/sections/News.tsx
+++ resources/js/Pages/Home/sections/News.tsx
@@ -1,11 +1,10 @@
 import NewsCarousel from '@/Components/Home/NewsCarousel';
 import { Berita } from '@/models/newsinterfaces';
 import Gallery from './Gallery';
-import NewPost from './NewPost';
 
 const News = ({ berita }: { berita: Berita[] }) => {
     return (
-        <div className="relative z-10 -mt-28 flex h-full w-full flex-col items-center justify-center gap-44 bg-[url(/images/bg-NewsHome.webp)] bg-cover bg-center bg-no-repeat pb-40 pt-[55rem] font-poppins">
+        <div className="relative z-10 -mt-28 flex h-full w-full flex-col items-center justify-center gap-44 bg-[url(/images/bg-NewsHome.webp)] bg-cover bg-top bg-no-repeat pb-40 pt-[45rem] font-poppins">
             <div className="z-10 -mt-80 flex w-[80%] flex-col items-center justify-center gap-9">
                 <h1 className="font-poppins text-4xl font-extrabold text-white">
                     Berita Terbaru
@@ -14,7 +13,6 @@ const News = ({ berita }: { berita: Berita[] }) => {
                     <NewsCarousel news={berita} />
                 </div>
             </div>
-            <NewPost />
             <Gallery />
         </div>
     );

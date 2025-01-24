3d300ea682180c171b5e7f8ec85f03b31113479c
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-24 18:00:13 +0700

    feat(detail-unit): add error handling for profile image with fallback mechanism

--- resources/js/Pages/DetailUnit/sections/Profile.tsx
+++ resources/js/Pages/DetailUnit/sections/Profile.tsx
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
+
 interface ProfileProps {
     gambar: string;
     isi: string;
     pembuka: string;
 }
 
+const fallbackImage = '/images/bg-LandingHome.webp'; 
+
 const Profile = ({ gambar, isi, pembuka }: ProfileProps) => {
+    const [imageSource, setImageSource] = useState(gambar);
+    const handleImageError = () => {
+        setImageSource(fallbackImage);
+    };
+
     return (
         <div className="flex flex-col items-center justify-center gap-12">
             <h1 className="text-3xl font-extrabold text-deep-blue">
@@ -16,14 +25,15 @@ const Profile = ({ gambar, isi, pembuka }: ProfileProps) => {
                         <div className="absolute inset-0 translate-x-4 translate-y-4 transform rounded-lg bg-blue-600" />
 
                         <img
-                            src={gambar}
+                            src={imageSource}
                             alt="Sekolah"
+                            onError={handleImageError}
                             className="relative z-10 h-auto w-full rounded-lg"
                         />
                     </div>
                 </div>
-                <div className="flex w-1/2 flex-col items-start justify-center gap-5">
-                    <p className="text-justify leading-relaxed">{pembuka}</p>
+                <div className="flex w-1/2 flex-col items-start justify-center gap-5 text-deep-blue">
+                    <p className="text-justify leading-relaxed font-extrabold">{pembuka}</p>
                     <p className="text-justify leading-relaxed">{isi}</p>
                 </div>
             </div>

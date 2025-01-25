97b6ae61c9af6f2fa0ac9e498af5362f5a9fb521
mamelilea <achmalpradiptaaditama@gmail.com>
2025-01-25 07:17:43 +0700

    feat(home): integrate Gallery section on homepage with backend data

--- resources/js/Pages/Home/sections/News.tsx
+++ resources/js/Pages/Home/sections/News.tsx
@@ -1,8 +1,9 @@
 import NewsCarousel from '@/Components/Home/NewsCarousel';
 import { Berita } from '@/models/newsinterfaces';
+import { GalleryItem } from '@/models/galleryinterfaces';
 import Gallery from './Gallery';
 
-const News = ({ berita }: { berita: Berita[] }) => {
+const News = ({ berita, galleryPhotos }: { berita: Berita[], galleryPhotos: GalleryItem[] }) => {
     return (
         <div className="relative z-10 -mt-28 flex h-full w-full flex-col items-center justify-center gap-44 bg-[url(/images/bg-NewsHome.webp)] bg-cover bg-top bg-no-repeat pb-40 pt-[45rem] font-poppins">
             <div className="z-10 -mt-80 flex w-[80%] flex-col items-center justify-center gap-9">
@@ -13,7 +14,7 @@ const News = ({ berita }: { berita: Berita[] }) => {
                     <NewsCarousel news={berita} />
                 </div>
             </div>
-            <Gallery />
+            <Gallery photos={galleryPhotos} />
         </div>
     );
 };

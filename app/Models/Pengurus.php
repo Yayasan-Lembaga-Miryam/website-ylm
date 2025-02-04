60bc6c79192c49966e6850af059ec07d88303af8
nathakusuma <nathakusuma07@gmail.com>
2025-02-05 01:03:57 +0700

    feat(pengurus): merge Pengurus and PengurusUnit model

--- app/Models/Pengurus.php
+++ app/Models/Pengurus.php
@@ -2,24 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Storage;
 
 class Pengurus extends Model
 {
-    const CREATED_AT = null;
+    use HasFactory;
 
     protected $table = 'pengurus';
 
     protected $fillable = [
+        'category',
+        'unit_id',
         'nama',
         'jabatan',
         'keterangan_jabatan',
+        'foto_path',
+        'prioritas',
     ];
 
     protected $casts = [
+        'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
+
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
+    }
 
     public function toArray()
     {

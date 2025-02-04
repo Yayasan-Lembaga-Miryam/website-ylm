ca88df573d9069d5531bd4dd68b08bf0d4ce156f
mamelilea <achmalpradiptaaditama@gmail.com>
2025-02-04 09:39:47 +0700

    feat(footer): add mailto link for email contact

--- resources/js/Components/Shared/Footer.tsx
+++ resources/js/Components/Shared/Footer.tsx
@@ -5,32 +5,39 @@ import { MdFax } from 'react-icons/md';
 
 const Footer = () => {
     return (
-        <div className="z-50 flex md:h-80 h-full w-full items-center justify-center bg-dark-blue">
-            <div className="flex h-full w-[80%] flex-col items-center justify-between gap-10 py-10 md:py-14 text-white">
-                <div className="flex md:flex-row flex-col w-full items-center md:justify-between md:gap-0 gap-3">
-                    <div className="md:w-1/3 space-y-3 flex flex-col items-center">
-                        <img src="/images/img-LogoText.png" alt="Logo YLM" className='md:w-full w-[80%]'/>
-                        <p className="md:text-justify md:text-sm text-[10px] text-center">
+        <div className="z-50 flex h-full w-full items-center justify-center bg-dark-blue md:h-80">
+            <div className="flex h-full w-[80%] flex-col items-center justify-between gap-10 py-10 text-white md:py-14">
+                <div className="flex w-full flex-col items-center gap-3 md:flex-row md:justify-between md:gap-0">
+                    <div className="flex flex-col items-center space-y-3 md:w-1/3">
+                        <img
+                            src="/images/img-LogoText.png"
+                            alt="Logo YLM"
+                            className="w-[80%] md:w-full"
+                        />
+                        <p className="text-center text-[10px] md:text-justify md:text-sm">
                             Jl. Patimura No.36, Gunung Mas, Kec. Tlk. Betung
                             Utara, Kota Bandar Lampung, Lampung 35011
                         </p>
                     </div>
                     <div className="flex h-full flex-col items-start justify-between gap-3">
-                        <div className="flex flex-col md:items-start items-center md:gap-3 gap-0">
-                            <div className="flex items-center gap-2 md:text-sm text-[10px]">
-                                <IoMdMail />
-                                <span>Email : lembagamiryam@gmail.com</span>
-                            </div>
-                            <div className="flex items-center gap-2 md:text-sm text-[10px]">
+                        <div className="flex flex-col items-center gap-0 md:items-start md:gap-3">
+                                <a
+                                    href="mailto:lembagamiryam@gmail.com"
+                                    className="flex items-center gap-2 text-[10px] md:text-sm"
+                                >
+                                    <IoMdMail />
+                                    <span>Email : lembagamiryam@gmail.com</span>
+                                </a>
+                            <div className="flex items-center gap-2 text-[10px] md:text-sm">
                                 <FaPhoneAlt />
                                 <span>Telp : 0721481934</span>
                             </div>
-                            <div className="flex items-center gap-2 md:text-sm text-[10px]">
+                            <div className="flex items-center gap-2 text-[10px] md:text-sm">
                                 <MdFax />
-                                <span>Fax : 35211</span>
+                                <span>Kode Pos : 35011</span>
                             </div>
                         </div>
-                        <div className="flex justify-center md:justify-start md:w-max w-full gap-3">
+                        <div className="flex w-full justify-center gap-3 md:w-max md:justify-start">
                             <a
                                 href=" https://www.youtube.com/@yayasanlembagamiryam"
                                 rel="noopener"
@@ -50,7 +57,7 @@ const Footer = () => {
                                 <CiInstagram className="size-5" />
                             </a>
                             <a
-                                href="https://wa.me/0885136126774"
+                                href="https://wa.me/+62885136126774"
                                 rel="noopener"
                                 target="_blank"
                                 title="link whatsapp"
@@ -61,7 +68,7 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex md:text-base text-center md:text-start text-[10px] w-full justify-center">
+                <div className="flex w-full justify-center text-center text-[10px] md:text-start md:text-base">
                     © 2024 Yayasan Lembaga Miryam | Designed & developed by IT
                     Partnership
                 </div>

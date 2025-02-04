69efceaa6c3f27970c5488af7888e2dd1cc9798b
nathakusuma <nathakusuma07@gmail.com>
2025-02-05 01:04:43 +0700

    feat(pengurus): adjust PengurusSeeder to the new design

--- database/seeders/PengurusSeeder.php
+++ database/seeders/PengurusSeeder.php
@@ -14,6 +14,7 @@ class PengurusSeeder extends Seeder
     public function run(): void
     {
         $pengurus = [
+            // Tree
             [
                 'id' => 1,
                 'nama' => 'John Doe',
@@ -49,24 +50,122 @@ class PengurusSeeder extends Seeder
                 'keterangan_jabatan' => '(Kepala Bidang Hukum)',
                 'foto_path' => 'dummy/johndoejr.jpg',
             ],
+
+            // Kepegawaian
             [
                 'id' => 6,
-                'nama' => 'Jane Doe Jr.',
-                'jabatan' => 'Kepala Satuan Pendidikan Inklusif',
-                'foto_path' => 'dummy/janedoejr.jpg',
+                'nama' => 'John Doe',
+                'jabatan' => 'Kepala Subbidang Tata Usaha & Administrasi Pegawai',
+                'foto_path' => 'dummy/johndoe.jpg',
             ],
             [
                 'id' => 7,
-                'nama' => 'John Smith Jr.',
-                'jabatan' => 'Kepala Subbidang Pengembangan',
-                'foto_path' => 'dummy/johnsmithjr.jpg',
+                'nama' => 'Jane Doe',
+                'jabatan' => 'Kepala Subbidang Pengadaan & Penilaian Kerja',
+                'foto_path' => 'dummy/janedoe.jpg',
             ],
             [
                 'id' => 8,
-                'nama' => 'Jane Smith Jr.',
+                'nama' => 'John Smith',
+                'jabatan' => 'Kepala Subbidang Pengolahan Data & Formasi',
+                'foto_path' => 'dummy/johnsmith.jpg',
+            ],
+            [
+                'id' => 9,
+                'nama' => 'Jane Smith',
+                'jabatan' => 'Kepala Subbidang Payroll',
+                'foto_path' => 'dummy/janesmith.jpg',
+            ],
+            [
+                'id' => 10,
+                'nama' => 'John Doe Jr.',
+                'jabatan' => 'Kepala Subbidang BPJS & DP KWI',
+                'foto_path' => 'dummy/johndoejr.jpg',
+            ],
+
+            // Keuangan
+            [
+                'id' => 11,
+                'nama' => 'John Doe',
+                'jabatan' => 'Kepala Subbidang Akunting',
+                'foto_path' => 'dummy/johndoe.jpg',
+            ],
+            [
+                'id' => 12,
+                'nama' => 'Jane Doe',
+                'jabatan' => 'Kepala Subbidang Budgeting',
+                'foto_path' => 'dummy/janedoe.jpg',
+            ],
+            [
+                'id' => 13,
+                'nama' => 'John Smith',
+                'jabatan' => 'Kepala Subbidang Sarpras & Umum',
+                'foto_path' => 'dummy/johnsmith.jpg',
+            ],
+            [
+                'id' => 14,
+                'nama' => 'Jane Smith',
+                'jabatan' => 'Tenaga Pelaksana Subbidang Sarpras & Umum',
+                'foto_path' => 'dummy/janesmith.jpg',
+            ],
+            [
+                'id' => 15,
+                'nama' => 'John Doe Jr.',
+                'jabatan' => 'Tenaga Pelaksana Subbidang Sarpras & Umum',
+                'foto_path' => 'dummy/johndoejr.jpg',
+            ],
+
+            // Akademik
+            [
+                'id' => 16,
+                'nama' => 'John Doe',
                 'jabatan' => 'Kepala Subbidang Kurikulum',
-                'foto_path' => 'dummy/janesmithjr.jpg',
-            ]
+                'foto_path' => 'dummy/johndoe.jpg',
+            ],
+            [
+                'id' => 17,
+                'nama' => 'Jane Doe',
+                'jabatan' => 'Kepala Subbidang Pengembangan',
+                'foto_path' => 'dummy/janedoe.jpg',
+            ],
+            [
+                'id' => 18,
+                'nama' => 'John Smith',
+                'jabatan' => 'Staf Pengembangan Spiritual',
+                'foto_path' => 'dummy/johnsmith.jpg',
+            ],
+            [
+                'id' => 19,
+                'nama' => 'Jane Smith',
+                'jabatan' => 'Kepala Subbidang Humas',
+                'foto_path' => 'dummy/janesmith.jpg',
+            ],
+            [
+                'id' => 20,
+                'nama' => 'John Doe Jr.',
+                'jabatan' => 'Staf Marketing',
+                'foto_path' => 'dummy/johndoejr.jpg',
+            ],
+            [
+                'id' => 21,
+                'nama' => 'Jane Doe Jr.',
+                'jabatan' => 'Staf Dokumentasi & Publikasi',
+                'foto_path' => 'dummy/janedoejr.jpg',
+            ],
+
+            // Hukum
+            [
+                'id' => 22,
+                'nama' => 'John Doe',
+                'jabatan' => 'Kepala Subbidang Regulasi',
+                'foto_path' => 'dummy/johndoe.jpg',
+            ],
+            [
+                'id' => 23,
+                'nama' => 'Jane Doe',
+                'jabatan' => 'Kepala Subbidang Perlindungan',
+                'foto_path' => 'dummy/janedoe.jpg',
+            ],
         ];
 
         foreach ($pengurus as $p) {

2fd3507423eab4d963f927bc24f83d699e0bba56
nathakusuma <nathakusuma07@gmail.com>
2025-02-13 00:21:07 +0700

    feat(home): add instagram embed in 'Postingan Terbaru' section

--- resources/js/Pages/Home/sections/News.tsx
+++ resources/js/Pages/Home/sections/News.tsx
@@ -1,19 +1,27 @@
 import NewsCarousel from '@/Components/Home/NewsCarousel';
-import { Berita } from '@/models/newsinterfaces';
 import { GalleryItem } from '@/models/galleryinterfaces';
+import { Berita } from '@/models/newsinterfaces';
+import NewPost from '@/Pages/Home/sections/NewPost';
 import Gallery from './Gallery';
 
-const News = ({ berita, galleryPhotos }: { berita: Berita[], galleryPhotos: GalleryItem[] }) => {
+const News = ({
+    berita,
+    galleryPhotos,
+}: {
+    berita: Berita[];
+    galleryPhotos: GalleryItem[];
+}) => {
     return (
-        <div className="relative z-10 -mt-28 flex h-full w-full flex-col items-center justify-center md:gap-44 gap-10 bg-[url(/images/bg-NewsHome.webp)] bg-cover bg-top bg-no-repeat pb-20 md:pb-40 md:pt-[45rem] pt-[38rem] font-poppins">
+        <div className="relative z-10 -mt-28 flex h-full w-full flex-col items-center justify-center gap-10 bg-[url(/images/bg-NewsHome.webp)] bg-cover bg-top bg-no-repeat pb-20 pt-[38rem] font-poppins md:gap-44 md:pb-40 md:pt-[45rem]">
             <div className="z-10 -mt-80 flex w-[80%] flex-col items-center justify-center md:gap-9">
-                <h1 className="font-poppins text-2xl md:text-4xl font-extrabold text-white">
+                <h1 className="font-poppins text-2xl font-extrabold text-white md:text-4xl">
                     Berita Terbaru
                 </h1>
                 <div className="w-[90%]">
                     <NewsCarousel news={berita} />
                 </div>
             </div>
+            <NewPost />
             <Gallery photos={galleryPhotos} />
         </div>
     );
